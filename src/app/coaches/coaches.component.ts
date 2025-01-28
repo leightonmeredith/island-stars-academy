b03246f914e0963e5acb2d86d36a87c98ae76fdf
Leighton Meredith <luluket2000@gmail.com>
2025-01-28 12:48:30 -0500

    add coach photo

--- src/app/coaches/coaches.component.ts
+++ src/app/coaches/coaches.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-coaches',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './coaches.component.html',
   styleUrl: './coaches.component.scss'
 })
 export class CoachesComponent {
 
+  coachesInfo = [
+    {src: '../assets/img/coaches/leighton.jpg', name: 'Leighton "M" Meredith', title: 'Coach', alt: 'profile'},
+    {src: '../assets/img/coaches/dwayne.jpg', name: 'Dwayne "Zaro" Thomas', title: 'Coach', alt: 'coordinator Shauna profile'},
+    {src: '../assets/img/coaches/malcom.jpg', name: 'Malcom ...', title: 'Coach', alt: 'coordinator Shauna profile'},
+    {src: '../assets/img/coaches/shauna.jpg', name: 'Shauna Langley', title: 'Academy Coordinator', alt: 'coordinator Shauna profile'}
+  ]
 }
